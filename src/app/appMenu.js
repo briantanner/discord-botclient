6f58b2073aa691dfb80376b0099e75a97d9717b2
Evelyne Lachance <eslachance@gmail.com>
2016-06-20 01:10:25 -0400

    Added dev tools menu and shortcut

--- src/app/appMenu.js
+++ src/app/appMenu.js
@@ -1,4 +1,8 @@
 "use strict";
+const electron = require('electron')
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const app = electron.app
 
 module.exports = function (app) {
   return [{
@@ -7,6 +11,40 @@ module.exports = function (app) {
       { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
       { type: "separator" },
       { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+      label: "View",
+      submenu: [
+      /*{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        // on reload, start fresh and close any old
+        // open secondary windows
+        if (focusedWindow.id === 1) {
+          BrowserWindow.getAllWindows().forEach(function (win) {
+            if (win.id > 1) {
+              win.close()
+            }
+          })
+        }
+        focusedWindow.reload()
+      }
+    }},*/ {
+    label: 'Toggle Developer Tools',
+    accelerator: (function () {
+      if (process.platform === 'darwin') {
+        return 'Alt+Command+I'
+      } else {
+        return 'Ctrl+Shift+I'
+      }
+    })(),
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.toggleDevTools()
+      }
+    }
+  }
     ]}, {
       label: "Edit",
       submenu: [
